4a77759e30b6412fd56f0094afca9621deb06b29
Jerome Perera <59548615+jerome-perera@users.noreply.github.com>
2022-10-11 03:08:24 -0400

    Update Intersection.py

--- Intersection.py
+++ Intersection.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import Trajectory
+
 class Intersection:
     def __init__(self, ID, name, center_point_location, num_legs, legs_collection):
         self.ID = ID
@@ -5,3 +8,7 @@ class Intersection:
         self.center_point_location = center_point_location
         self.num_legs = num_legs
         self.legs_collection = legs_collection
+
+    def plot_trajectory():
+        plt.plot(Trajectory.compute_xtrajectory(Trajectory.departing_lane, Trajectory.receiving_lane),
+         Trajectory.compute_ytrajectory(Trajectory.departing_lane, Trajectory.receiving_lane))
