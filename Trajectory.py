1a33a7d6eeb379d8638862473d9b5f9b0981205b
Jerome Perera <59548615+jerome-perera@users.noreply.github.com>
2022-10-15 20:47:18 -0400

    Update Trajectory.py

--- Trajectory.py
+++ Trajectory.py
@@ -1,5 +1,6 @@
 import Lane
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Trajectory:
     def __init__(self, trajection_geometry):
@@ -7,14 +8,12 @@ class Trajectory:
         self.receiving_lane = Lane()
         self.trajection_geometry = trajection_geometry
 
-    def compute_xtrajectory(self, departing_lane, receiving_lane):
+    def compute_trajectory(self, departing_lane, receiving_lane):
+        arr = np.zeros((2, 3))
         (x1, y1) = departing_lane.intersection_edge_point
         (x2, y2) = receiving_lane.intersection_edge_point
-        xcoord = [x1, x2]
-        return xcoord
-
-    def compute_ytrajectory(self, departing_lane, receiving_lane):
-        (x1, y1) = departing_lane.intersection_edge_point
-        (x2, y2) = receiving_lane.intersection_edge_point
-        ycoord = [y1, y2]
-        return ycoord
+        arr[0, 0] = x1
+        arr[1, 0] = y1
+        arr[0, 1] = x2
+        arr[1, 1] = y2
+        return arr
