383e44b9b6280d400c8a3d4025670981a0f53e10
Jerome Perera <59548615+jerome-perera@users.noreply.github.com>
2023-03-30 19:13:55 -0400

    Update Output.py

--- Output.py
+++ Output.py
@@ -8,19 +8,19 @@ class Output:
     def init(self, id):
         self.id = id
 
-    lane1 = Lane("ID1", 30, (110, 70), "departing")
-    lane2 = Lane("ID2", 30, (110, 70), "receiving")
-    lane3 = Lane("ID3", 30, (80, 100), "departing")
-    lane4 = Lane("ID4", 30, (80, 100), "receiving")
-    lane5 = Lane("ID5", 30, (50, 70), "departing")
-    lane6 = Lane("ID6", 30, (50, 70), "receiving")
-    lane7 = Lane("ID7", 30, (80, 40), "departing")
-    lane8 = Lane("ID8", 30, (80, 40), "receiving")
+    lane1 = Lane("ID1", 30, (30, 0), "departing")
+    lane2 = Lane("ID2", 30, (30, 0), "receiving")
+    lane3 = Lane("ID3", 30, (0, 30), "departing")
+    lane4 = Lane("ID4", 30, (0, 30), "receiving")
+    lane5 = Lane("ID5", 30, (-30, 0), "departing")
+    lane6 = Lane("ID6", 30, (-30, 0), "receiving")
+    lane7 = Lane("ID7", 30, (0, -30), "departing")
+    lane8 = Lane("ID8", 30, (0, -30), "receiving")
     leg1 = Leg("IDL1", "right", 2, (lane1, lane2))
     leg2 = Leg("IDL2", "top", 2, (lane3, lane4))
     leg3 = Leg("IDL3", "left", 2, (lane5, lane6))
     leg4 = Leg("IDL4", "bottom", 2, (lane7, lane8))
-    intersection1 = Intersection("ID", "name", (80, 70), 4, (leg1, leg2, leg3, leg4))
+    intersection1 = Intersection("ID", "name", (100, 100), 4, (leg1, leg2, leg3, leg4))
     
     right_lane_edge_point = lane1.intersection_edge_point
     top_lane_edge_point = lane3.intersection_edge_point
